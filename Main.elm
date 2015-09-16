2c1ffc6c365bd15d220fd4e30aceeafe5b8f7ea5
Jesse Williamson <jessewilliamson@fastmail.fm>
2015-09-16 15:29:05 -0700

    making it pretty

--- Main.elm
+++ Main.elm
@@ -1,5 +1,6 @@
 import StartApp
 import Task
+import Html
 import Effects exposing (Never)
 
 import FlickrViewer exposing (..)
@@ -12,6 +13,7 @@ app = StartApp.start
       }
 
 
+main : Signal Html.Html
 main = app.html
 
 port tasks : Signal (Task.Task Never ())
